1754472d2d2b7cfad57b75adb92fdcfb7817dcd3
Eric Kang <erickangatms@outlook.com>
2017-05-10 12:10:33 -0700

    formatting

--- backup/backup.sql
+++ backup/backup.sql
@@ -1,5 +1,7 @@
 BACKUP DATABASE [AdventureWorks] TO  
-DISK = N'G:\sqlbackups\Device1\AdventureworkdDevice1.bak',  DISK = N'G:\sqlbackups\Device3\AdventureWorksDevice3.bak',  DISK = N'G:\sqlbackups\Device2\AdventureWorksDevice2.bak' 
+DISK = N'G:\sqlbackups\Device1\AdventureworkdDevice1.bak',  
+DISK = N'G:\sqlbackups\Device3\AdventureWorksDevice3.bak',  
+DISK = N'G:\sqlbackups\Device2\AdventureWorksDevice2.bak' 
 WITH NOFORMAT, NOINIT,  
 NAME = N'AdventureWorks-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
 GO
