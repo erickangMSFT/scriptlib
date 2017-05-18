0d3c6d347258662d67b3cc0669ac9e30e409e9e1
Eric Kang <erickangatms@outlook.com>
2017-05-17 19:54:52 -0700

    failed login audit log

--- security/serveraudit/readauditlog.sql
+++ security/serveraudit/readauditlog.sql
@@ -27,13 +27,12 @@ SELECT
     event_time, 
     action_id, 
     succeeded, 
-    database_name,
-    schema_name,
-    object_name,
+    database_name + '.' + schema_name + '.' + object_name as 'object name',
     client_ip,
     application_name,
     server_principal_name, database_principal_name,
     [statement]
-FROM sys.fn_get_audit_file ('/var/opt/mssql/auditlog/CarbonAudit_FC062890-A102-46B3-83CC-CD148AB70EB2_0_131394759189390000.sqlaudit',default,default);    
+FROM sys.fn_get_audit_file ('/var/opt/mssql/auditlog/CarbonAudit_FC062890-A102-46B3-83CC-CD148AB70EB2_0_131394759189390000.sqlaudit',default,default)    
+WHERE succeeded = 0
 GO  
 
