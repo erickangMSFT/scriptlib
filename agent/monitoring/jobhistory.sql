1012c173cb42290e54d8d6bf4a49d60d97546752
Eric Kang <erickang@microsoft.com>
2018-02-15 11:43:27 -0800

    .

--- agent/monitoring/jobhistory.sql
+++ agent/monitoring/jobhistory.sql
@@ -1,2 +1,7 @@
 --EXEC msdb.dbo.sp_help_jobhistory
-select * from [msdb].[dbo].[sysjobhistory]
+select job_id, run_date, run_time, run_status, [message] from [msdb].[dbo].[sysjobhistory]
+where step_id = 0
+
+select job_id, run_date, run_time, run_duration from msdb.dbo.sysjobhistory
+
+select * from msdb.dbo.sysalerts
